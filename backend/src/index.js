500a460d4f333278b9f838f2ac7c957942533b67
Deepanshu <deepanshusaxena315@gmail.com>
2026-01-26 18:46:24 +0530

    login apis created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -7,8 +8,12 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
 const PORT = process.env.PORT || 5001;
 app.use('/api/auth', authRoutes);
+
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:"+PORT);
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { login, signup, logout } from "../controllers/auth.controller.js";
+import { login, signup, logout, updateProfile} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { checkAuth } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +10,9 @@ router.post("/login", login);
 router.post("/signup", signup); 
 
 router.post("/logout", logout);
+
+router.post("/update-profile",protectRoute, updateProfile);
+
+router.get("/check", protectRoute,checkAuth);
 
 export default router;
