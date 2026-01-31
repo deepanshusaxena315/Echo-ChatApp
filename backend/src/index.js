3221cd32d81a2e1354bd9d31db608778d7f6a2f5
Deepanshu <deepanshusaxena315@gmail.com>
2026-01-31 16:50:41 +0530

    message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(cookieParser());
 
 const PORT = process.env.PORT || 5001;
 app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
 
 
 app.listen(PORT, () => {
