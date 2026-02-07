af8f1e6002d4d44314586d189031095604212bd0
Deepanshu <deepanshusaxena315@gmail.com>
2026-02-07 19:23:54 +0530

    Login and signup pages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
@@ -11,6 +12,10 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 
 const PORT = process.env.PORT || 5001;
 app.use('/api/auth', authRoutes);
